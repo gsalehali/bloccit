8fbab98fc4d1fdb501ec2fc9a5ed6816cffba9c0
gsalehali <me@salehali.net>
2016-01-24 10:43:44 +0300

    Assignment 32 seed data: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,30 @@ require 'faker'
 # Create Posts 
 
 50.times do 
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body:  Faker::Lorem.paragraph
-  )
+  title = Faker::Lorem.sentence
+  post_body = Faker::Lorem.paragraph
+  if !Post.exists?(title: title, body: post_body)  
+    Post.create!(
+      title: title,
+      body: post_body
+      )
+  end
 end
+
 posts = Post.all
 
 # Create Comments
 100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
+  post = posts.sample
+  comment_body = Faker::Lorem.paragraph
+  if !Comment.exists?(post: post, body: comment_body)  
+    Comment.create!(
+      post: post,
+      body: comment_body
   )
+  end
 end
+
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
