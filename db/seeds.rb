84fd751d9b149a4d8c1f804a97244da71664fee3
gsalehali <me@salehali.net>
2016-01-27 10:52:11 +0300

    Checkpoint 38 associations: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
 require 'faker'
 
-# Create Posts 
+# Creat Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
 
+users = User.all
+
+
+# Create Posts 
 50.times do 
   Post.create!(
+    user:  users.sample,
     title: Faker::Lorem.sentence,
     body:  Faker::Lorem.paragraph
   )
@@ -18,6 +32,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: 'test@test.test',
+  password: 'testtest'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
