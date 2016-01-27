5f66b44b18ef84aea14b5014eab53b3c2330c791
gsalehali <me@salehali.net>
2016-01-28 00:16:24 +0300

    Assignment 39 authorization: completed

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,24 @@
 class PostPolicy < ApplicationPolicy
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.present? && (user.admin? || user.moderator?)
+        scope.all
+      elsif user.present?
+        scope.where(user_id: user)
+      else
+        return []
+      end
+    end
+
+  end
+
   def index?
     true
   end
