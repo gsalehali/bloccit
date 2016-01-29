1754170c2123ff32e06cec1e3db3d17fb3d3a2df
gsalehali <me@salehali.net>
2016-01-30 00:05:03 +0300

    Checkpoint 44 uploads images: completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128051333) do
+ActiveRecord::Schema.define(version: 20160129170737) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20160128051333) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
