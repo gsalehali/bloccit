885b5be6100508d5b37cfa05db028a8715989864
gsalehali <me@salehali.net>
2016-02-09 22:59:41 +0300

    Checkpoint 50 mocking with RSpec: completed

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
 describe Vote do
+
+  include TestFactories
+
   describe "validations" do
     describe "value validation" do       
       it "only allows -1 or 1 as values" do
           value = Vote.new(value: 2)         
           expect(value.valid?).to eq(false)
       end
+    end
+  end
+
+  describe 'after_save' do
+    it "calls 'post#update_rand' after save" do
+      post = associated_post
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
     end
   end
 end
