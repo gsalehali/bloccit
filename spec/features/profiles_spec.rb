d7234af069cc3aecd42d0ed56d674c5467f9deb3
gsalehali <me@salehali.net>
2016-02-12 14:26:44 +0300

    Assignmetn 54 public profiles: completed

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 describe "Visiting profiles" do 
 
   include TestFactories
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
   before do
     @user = authenticated_user
@@ -22,6 +24,18 @@ describe "Visiting profiles" do
       expect( page ).to have_content(@post.title)
       expect( page ).to have_content(@comment.body)
     end
+  end
 
+  describe "signed in" do
+
+    it "shows users own profile" do
+      login_as(@user, :scope => :user)
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+    end
   end
 end
